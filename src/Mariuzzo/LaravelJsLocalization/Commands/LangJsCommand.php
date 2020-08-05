7429ae1d8da071afc5893d9c92f0bd9cdd568995
George Chaduneli <g.chaduneli94@gmail.com>
2020-08-05 22:52:48 +0400

    Initial commit

---
+++ src/Mariuzzo/LaravelJsLocalization/Commands/LangJsCommand.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Mariuzzo\LaravelJsLocalization\Commands;
+
+use Illuminate\Console\Command;
+use Mariuzzo\LaravelJsLocalization\Generators\LangJsGenerator;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * The LangJsCommand class.
+ *
+ * @author  Rubens Mariuzzo <rubens@mariuzzo.com>
+ */
+class LangJsCommand extends Command
+{
+    /**
+     * The command name.
+     *
+     * @var string
+     */
+    protected $name = 'lang:js';
+
+    /**
+     * The command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate JS lang files.';
+
+    /**
+     * The generator instance.
+     *
+     * @var LangJsGenerator
+     */
+    protected $generator;
+
+    /**
+     * Construct a new LangJsCommand.
+     *
+     * @param LangJsGenerator $generator The generator.
+     */
+    public function __construct(LangJsGenerator $generator)
+    {
+        $this->generator = $generator;
+        parent::__construct();
+    }
+
+    /**
+     * Fire the command.
+     */
+    public function fire()
+    {
+        $target = $this->argument('target');
+        $options = ['compress' => $this->option('compress')];
+
+        if ($this->generator->generate($target, $options)) {
+            $this->info("Created: {$target}");
+
+            return;
+        }
+
+        $this->error("Could not create: {$target}");
+    }
+
+    /**
+     * Return all command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['target', InputArgument::OPTIONAL, 'Target path.', config("localization-js.default_target_path", $this->getPublicPath().'/messages.js')],
+        ];
+    }
+
+    /**
+     * Return all command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['compress', 'c', InputOption::VALUE_NONE, 'Compress the JavaScript file.', null],
+        ];
+    }
+
+    /**
+     * Return the public path of the Laravel application.
+     *
+     * @return string
+     */
+    public function getPublicPath()
+    {
+        return public_path();
+    }
+}
---
+++ src/Mariuzzo/LaravelJsLocalization/Generators/LangJsGenerator.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Mariuzzo\LaravelJsLocalization\Generators;
+
+use Illuminate\Filesystem\Filesystem as File;
+use JShrink\Minifier;
+
+/**
+ * The LangJsGenerator class.
+ *
+ * @author  Rubens Mariuzzo <rubens@mariuzzo.com>
+ */
+class LangJsGenerator
+{
+
+    /**
+     * The file service.
+     *
+     * @var File
+     */
+    protected $file;
+
+    /**
+     * The source path of the language files.
+     *
+     * @var string
+     */
+    protected $sourcePath;
+
+    /**
+     * List of messages should be included in build.
+     *
+     * @var array
+     */
+    protected $messagesIncluded = [];
+
+
+    /**
+     * Construct a new LangJsGenerator instance.
+     *
+     * @param File   $file       The file service instance.
+     * @param string $sourcePath The source path of the language files.
+     */
+    public function __construct(File $file, $sourcePath, $messagesIncluded = [])
+    {
+        $this->file             = $file;
+        $this->sourcePath       = $sourcePath;
+        $this->messagesIncluded = $messagesIncluded;
+    }
+
+
+    /**
+     * Generate a JS lang file from all language files.
+     *
+     * @param string $target  The target directory.
+     * @param array  $options Array of options.
+     *
+     * @return int
+     */
+    public function generate($target, $options)
+    {
+        $messages = $this->getMessages();
+        $this->prepareTarget($target);
+
+        $template = $this->file->get(__DIR__ . '/Templates/langjs_with_messages.js');
+        $langjs   = $this->file->get(__DIR__ . '/../../../../lib/lang.min.js');
+
+        $template = str_replace('\'{ messages }\'', json_encode($messages), $template);
+        $template = str_replace('\'{ langjs }\';', $langjs, $template);
+
+        if ($options['compress']) {
+            $template = Minifier::minify($template);
+        }
+
+        return $this->file->put($target, $template);
+    }
+
+
+    /**
+     * Return all language messages.
+     *
+     * @return array
+     *
+     * @throws \Exception
+     */
+    protected function getMessages()
+    {
+        $messages = [];
+        $path     = $this->sourcePath;
+
+        if ( ! $this->file->exists($path)) {
+            throw new \Exception("${path} doesn't exists!");
+        }
+
+        foreach ($this->file->allFiles($path) as $file) {
+            $pathName = $file->getRelativePathName();
+
+            if ($this->file->extension($pathName) !== 'php') {
+                continue;
+            }
+
+            if ($this->isMessagesExcluded($pathName)) {
+                continue;
+            }
+
+            $key = substr($pathName, 0, -4);
+            $key = str_replace('\\', '.', $key);
+            $key = str_replace('/', '.', $key);
+
+            $messages[$key] = include $path . DIRECTORY_SEPARATOR . $pathName;
+        }
+
+        foreach ($this->file->allFiles(resource_path('lang')) as $file) {
+            $pathName = $file->getRelativePathName();
+
+            if ($this->file->extension($pathName) !== 'json') {
+                continue;
+            }
+            $key                     = substr($pathName, 0, -4);
+            $key                     = str_replace('\\', '.', $key);
+            $key                     = str_replace('/', '.', $key);
+            $messages[$key . 'json'] = json_decode(file_get_contents($file->getRealPath()));
+        }
+
+        return $messages;
+    }
+
+
+    /**
+     * If messages should be excluded from build.
+     *
+     * @param string $filePath
+     *
+     * @return bool
+     */
+    protected function isMessagesExcluded($filePath)
+    {
+        if (empty($this->messagesIncluded)) {
+            return false;
+        }
+
+        $filePath = str_replace(DIRECTORY_SEPARATOR, '/', $filePath);
+
+        $localeDirSeparatorPosition = strpos($filePath, '/');
+        $filePath                   = substr($filePath, $localeDirSeparatorPosition);
+        $filePath                   = ltrim($filePath, '/');
+        $filePath                   = substr($filePath, 0, -4);
+
+        if (in_array($filePath, $this->messagesIncluded)) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Prepare the target directory.
+     *
+     * @param string $target The target directory.
+     */
+    protected function prepareTarget($target)
+    {
+        $dirname = dirname($target);
+
+        if ( ! $this->file->exists($dirname)) {
+            $this->file->makeDirectory($dirname, null, true);
+        }
+    }
+}
